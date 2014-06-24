88696a6b6f10bb61e8b142fa2cc01131d3ef00a1
admin-harold.rojas <admin-harold.rojas@FIA-LAB7PROF.lim.upeu.edu.pe>
2014-06-24 16:25:33 -0500

    version 3: mensaje

--- src/pe/edu/upeu/prueba/DAO/Tipo_IglesiaDAO.java
+++ src/pe/edu/upeu/prueba/DAO/Tipo_IglesiaDAO.java
@@ -6,10 +6,37 @@
 
 package pe.edu.upeu.prueba.DAO;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import pe.edu.upeu.prueba.config.Conexion;
+import pe.edu.upeu.prueba.modelo.Distrito;
+import pe.edu.upeu.prueba.modelo.Tipo_Iglesia;
+
 /**
  *
  * @author admin-harold.rojas
  */
 public class Tipo_IglesiaDAO {
-    
+    private ArrayList<Tipo_Iglesia> lista = new ArrayList();
+    Distrito d = new Distrito();
+    private String sql;
+    private Statement st;
+    private Connection cx;
+    private ResultSet rs;
+    public ArrayList<Tipo_Iglesia> listarTipo_Iglesia(){
+    sql = "SELECT *FROM Tipo_Iglesia";
+        try {
+            cx = Conexion.getConexion();
+            st = cx.createStatement();
+            rs = st.executeQuery(sql);
+            while(rs.next()){
+            lista.add(Tipo_Iglesia.loadTipo_Iglesia(rs));           
+            }
+        } catch (SQLException e) {            
+        }        
+    return lista;
+    }
 }
